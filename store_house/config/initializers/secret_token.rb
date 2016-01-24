519a6b8856c5298d2ad7c8ff2e936a749242d51f
Natacha Fernández <natacha.fernandez@sage.com>
2016-01-24 22:49:40 +0100

    first commit

---
+++ store_house/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreHouse::Application.config.secret_token = '930bf66d3c28482a8ce4d58e832d638e898049f32e51d86e3e152d6cb54fc88aa55ac07f4b2fd5561b78ce89503e320548ec3f3f080b8a1be9eb4c243dd9d644'
