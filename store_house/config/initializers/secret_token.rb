8229954a4f7ce9a0511a81ee7aad80d1a896c2eb
Natacha Fernández <natacha.fernandez@sage.com>
2016-01-24 22:51:07 +0100

    secret_token

--- store_house/config/initializers/secret_token.rb
+++ store_house/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-StoreHouse::Application.config.secret_token = '930bf66d3c28482a8ce4d58e832d638e898049f32e51d86e3e152d6cb54fc88aa55ac07f4b2fd5561b78ce89503e320548ec3f3f080b8a1be9eb4c243dd9d644'
+StoreHouse::Application.config.secret_token = '3c637b5d55e5295c8a0759a1c7a275e361b166bb4084d241f5472a3efc400e0f26a3a6c74a843c6bd7fc900d474b7d747151a42dc202cb33a238a8aab1c93adc'
