ffe74cfa2a61b70932db3c93177a921c0cf2603d
Natacha Fernández <natacha.fernandez@sage.com>
2016-02-03 18:04:13 +0100

    Update business.rb

--- store_house/app/models/business.rb
+++ store_house/app/models/business.rb
@@ -1,7 +1,7 @@
 class Business < ActiveRecord::Base
 
   # Associations
-  has_many :addresses, :as => :addressable
+  has_many :addresses, :class_name: 'Address', :as => :addressable
 
   # Attributes
   attr_accessible :name, :company, :tax_number, :email, :telephone, :mobile
