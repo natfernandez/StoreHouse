f2899ca8689ed478ad97a46dff82a6bc81d4e2a5
Natacha Fernández <natacha.fernandez@sage.com>
2016-02-08 21:00:25 +0100

    Added shared examples and gems for debug

---
+++ store_house/app/models/size_letter.rb
@@ -0,0 +1,3 @@
+class SizeLetter < Size
+
+end
--- store_house/spec/spec_helper.rb
+++ store_house/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -11,7 +12,6 @@ require 'rspec/rails'
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
 RSpec.configure do |config|
   # ## Mock Framework
   #
