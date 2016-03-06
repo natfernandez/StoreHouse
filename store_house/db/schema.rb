910870caa2b1caabc6e2b80cc0e6eabb2fa8a2b1
Natacha Fernández <natacha.fernandez@sage.com>
2016-03-06 22:14:34 +0100

    WIP

--- store_house/db/schema.rb
+++ store_house/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160207203750) do
+ActiveRecord::Schema.define(:version => 20160306183004) do
 
   create_table "addresses", :force => true do |t|
     t.string  "line_1",           :limit => 50
@@ -111,6 +111,16 @@ ActiveRecord::Schema.define(:version => 20160207203750) do
   end
 
   add_index "prices", ["priceable_id", "priceable_type"], :name => "index_prices_on_priceable_id_and_priceable_type", :unique => true
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sizes", :force => true do |t|
     t.string "description"
